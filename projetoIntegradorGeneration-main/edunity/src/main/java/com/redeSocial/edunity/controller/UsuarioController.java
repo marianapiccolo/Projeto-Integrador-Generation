465f0fd949da5f63de24851fe0213b4fe35597f4
jobremote <leonardo.n.souza@hotmail.com>
2021-03-01 12:21:23 -0300

    Criação de relações

--- projetoIntegradorGeneration-main/edunity/src/main/java/com/redeSocial/edunity/controller/UsuarioController.java
+++ projetoIntegradorGeneration-main/edunity/src/main/java/com/redeSocial/edunity/controller/UsuarioController.java
@@ -1,8 +1,6 @@
 package com.redeSocial.edunity.controller;
 
 import java.util.List;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.redeSocial.edunity.model.Usuario;
 import com.redeSocial.edunity.repository.UsuarioRepository;
 
@@ -23,37 +20,38 @@ import com.redeSocial.edunity.repository.UsuarioRepository;
 @RequestMapping("/usuario")
 @CrossOrigin("*")
 public class UsuarioController {
-	
-	 @Autowired 
-	 private UsuarioRepository usuarioRepository;
-	
-	 @GetMapping
-	 public ResponseEntity<List<Usuario>> GetAll(){
-		  return ResponseEntity.ok(usuarioRepository.findAll());
-	 }
-	 
-	 @GetMapping ("/{id}")
-	 public ResponseEntity<Usuario> GetById(@PathVariable long id){
-		 return usuarioRepository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
-	 }
-	
-    @GetMapping("/usuario/{nome}")
-    public ResponseEntity<List<Usuario>> GetByUsuario(@PathVariable String nome){
-    	return ResponseEntity.ok(usuarioRepository.findAllByNomeContainingIgnoreCase(nome));
-    }
-    
-    @PostMapping 
-    public ResponseEntity<Usuario> postUsuario (@RequestBody Usuario nome){
-    	return ResponseEntity.status(HttpStatus.CREATED).body(usuarioRepository.save(nome));
-    }
-    
-    @PutMapping
-    public ResponseEntity<Usuario> putUsuario (@RequestBody Usuario nome){
-    	return ResponseEntity.status(HttpStatus.CREATED).body(usuarioRepository.save(nome));
-    }
-    
-    @DeleteMapping ("/{id}")
-    public void deleteUsuario (@PathVariable long id) {
-    	usuarioRepository.deleteById(id);
-    }
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	@GetMapping
+	public ResponseEntity<List<Usuario>> GetAll() {
+		return ResponseEntity.ok(usuarioRepository.findAll());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> GetById(@PathVariable long id) {
+		return usuarioRepository.findById(id).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+
+	@GetMapping("/usuario/{nome}")
+	public ResponseEntity<List<Usuario>> GetByUsuario(@PathVariable String nome) {
+		return ResponseEntity.ok(usuarioRepository.findAllByNomeContainingIgnoreCase(nome));
+	}
+
+	@PostMapping
+	public ResponseEntity<Usuario> postUsuario(@RequestBody Usuario nome) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(usuarioRepository.save(nome));
+	}
+
+	@PutMapping
+	public ResponseEntity<Usuario> putUsuario(@RequestBody Usuario nome) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(usuarioRepository.save(nome));
+	}
+
+	@DeleteMapping("/{id}")
+	public void deleteUsuario(@PathVariable long id) {
+		usuarioRepository.deleteById(id);
+	}
 }
