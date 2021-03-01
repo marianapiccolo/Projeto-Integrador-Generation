67f473831d5feef5e18e9b0247be1282fc631b8d
ThalitaProgramadora <thalita.programadora@gmail.com>
2021-03-01 10:15:43 -0300

    criando todos os GetMapping

--- projetoIntegradorGeneration-main/edunity/src/main/java/com/redeSocial/edunity/controller/UsuarioController.java
+++ projetoIntegradorGeneration-main/edunity/src/main/java/com/redeSocial/edunity/controller/UsuarioController.java
@@ -1,10 +1,17 @@
 package com.redeSocial.edunity.controller;
 
+import java.util.List;
+
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.redeSocial.edunity.model.Usuario;
 import com.redeSocial.edunity.repository.UsuarioRepository;
 
 @RestController
@@ -15,8 +22,19 @@ public class UsuarioController {
 	 @Autowired 
 	 private UsuarioRepository usuarioRepository;
 	
-	 /*@GetMapping
-	 public ResponseEntity<>*/
+	 @GetMapping
+	 public ResponseEntity<List<Usuario>> GetAll(){
+		  return ResponseEntity.ok(usuarioRepository.findAll());
+	 }
+	 
+	 @GetMapping ("/{id}")
+	 public ResponseEntity<Usuario> GetById(@PathVariable long id){
+		 return usuarioRepository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	 }
 	
-
+    @GetMapping("/usuario/{usuario}")
+    public ResponseEntity<List<Usuario>> GetByUsuario(@PathVariable String nome){
+    	return ResponseEntity.ok(usuarioRepository.findAllByNameContainingIgnoreCase(nome));
+    }
+    
 }
